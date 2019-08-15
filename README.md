001d1a706ff7a48b8841050c3d1a801f4b432e17
Shlomi Laptop Ubuntu <shlomid@protonmail.com>
2019-08-15 15:43:15 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 IOT-Project
+
+# Test
---
+++ setup.sh
@@ -0,0 +1,6 @@
+sudo apt install npm nodejs
+sudo npm install -g --unsafe-perm node-red
+
+
+# To add git submodule:
+# git submodule add https://github.com/ShlomiRex/esp32-mqtt
