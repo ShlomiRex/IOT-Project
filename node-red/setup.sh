45df7379056dea55eac1f38daee99e8ebaa058eb
Shlomi <shlomid@protonmail.com>
2019-08-23 18:15:05 +0300

    Added UI flow

---
+++ node-red/setup.sh
@@ -0,0 +1,11 @@
+# Install BigML for node-red
+cd $HOME/.node-red
+npm install bigml-nodered
+
+sudo snap install mosquitto
+
+# You need to install:
+
+#bigml-nodered
+#mongoDB
+#node-red-dashboard
--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo npm install -g --unsafe-perm node-red
 sudo npm install node-red-node-mongodb
 
 bash esp32-mqtt/setup.sh
-
+bash node-red/setup.sh
 
 # To add git submodule:
 # git submodule add https://github.com/ShlomiRex/esp32-mqtt
